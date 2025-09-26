949f3997a1ccc35de39bc63f25fe2cbc9d3644e8
pinki <pinkas.matej@gmail.com>
2025-09-26 09:38:12 +0200

    added first homework solution: answer.py

---
+++ homeworks/01_DU_abc/answer.py
@@ -0,0 +1,19 @@
+__author__ = "Pinkas Matěj"
+__maintainer__ = "Pinkas Matěj"
+__email__ = "pinkas.matej@gmail.com"
+__created__ = "26/09/2025"
+__date__ = "26/09/2025"
+__status__ = "Prototype"
+__version__ = "0.1.0"
+
+"""
+Project: B4B33RPH
+Filename: answer.py
+Directory: homeworks/01_DU_abc/
+"""
+
+def main():
+    print('B\nA\nC')
+
+if __name__ == '__main__':
+    main()
