117ecaba3dec1395a81bba1cfdede2ae64ca9f2e
pinki <pinkas.matej@gmail.com>
2025-09-29 10:04:24 +0200

    commit after first lesson

---
+++ cv_1/doors.py
@@ -0,0 +1,28 @@
+__author__ = "Pinkas Matěj - Pinki"
+__maintainer__ = "Pinkas Matěj - Pinki"
+__email__ = "pinkas.matej@gmail.com"
+__credits__ = []
+__created__ = "26/09/2025"
+__date__ = "26/09/2025"
+__status__ = "Prototype"
+__version__ = "0.1.0"
+__copyright__ = ""
+__license__ = ""
+
+"""
+Project: B4B33RPH
+Filename: doors.py
+Directory: cv_1/
+"""
+
+import random
+
+def monty(switch_strategy):
+    doors = [0 for i in range(3)]
+    car = random.randint(0,3)
+    doors[car] = 1
+
+
+
+if __name__ == '__main__':
+    pass
---
+++ cv_1/prvnikod.py
@@ -0,0 +1,22 @@
+__author__ = "Pinkas Matěj - Pinki"
+__maintainer__ = "Pinkas Matěj - Pinki"
+__email__ = "pinkas.matej@gmail.com"
+__credits__ = []
+__created__ = "26/09/2025"
+__date__ = "26/09/2025"
+__status__ = "Prototype"
+__version__ = "0.1.0"
+__copyright__ = ""
+__license__ = ""
+
+"""
+Project: B4B33RPH
+Filename: prvnikod.py
+Directory: cv_1/
+"""
+
+def main():
+    print('ahoj')
+
+if __name__ == '__main__':
+    main()
